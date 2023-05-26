4d33300a01c8070faece9141e3da739672b7306d
javad <javadMohammadi.js@gmail.com>
2023-05-26 20:16:20 +0330

    add search result then show data with style

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,9 +1,42 @@
+import getWikiResults from '@/lib/getWikiResult';
+import Item from './components/Item';
 type Props = {
-  prams: { searchTerm: string };
+  params: { searchTerm: string };
 };
 
-function SearchResults({ prams: { searchTerm } }: Props) {
-  return <div>SearchResults</div>;
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+  const displayTerm = searchTerm.replaceAll('%20', ' ');
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${displayTerm} Not Found`,
+    };
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search results for ${displayTerm}`,
+  };
+}
+async function SearchResults({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+  const results: Result[] | undefined = data?.query?.pages;
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+      {results ? (
+        Object.values(results).map((result) => {
+          return <Item key={result.pageid} result={result} />;
+        })
+      ) : (
+        <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
+      )}
+    </main>
+  );
+
+  return content;
 }
 
 export default SearchResults;
--- types.d.ts
+++ types.d.ts
@@ -27,3 +27,19 @@ type User = {
     bs: string;
   };
 };
+type Result = {
+  pageid: string;
+  title: string;
+  extract: string;
+  thumbnail?: {
+    source: string;
+    width: number;
+    height: number;
+  };
+};
+
+type SearchResult = {
+  query?: {
+    pages?: Result[];
+  };
+};
