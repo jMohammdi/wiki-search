c5151970ae1314c147fbc4bb9976d5b22e1e9ac3
javad <javadMohammadi.js@gmail.com>
2023-05-26 18:55:55 +0330

    add search tem and dynamic search page

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  prams: { searchTerm: string };
+};
+
+function SearchResults({ prams: { searchTerm } }: Props) {
+  return <div>SearchResults</div>;
+}
+
+export default SearchResults;
